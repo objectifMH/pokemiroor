a92b12e73604c01ff668f42e9d0d33532d7c9545
unknown <objectifmh@hotmail.com>
2020-09-12 18:55:34 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokemirroor';
+
+  faLinkedinIn = faLinkedinIn;
+  faGithub = faGithub;
+  faUserCircle = faUserCircle;
 }
